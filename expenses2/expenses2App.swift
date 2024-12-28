1725d8db4a77aecb620c11be12e305013d309464
tttjjjzzz <tigerjzhou@gmail.com>
2024-12-28 13:27:08 +0800

    Initial Commit

---
+++ expenses2/expenses2App.swift
@@ -0,0 +1,17 @@
+//
+//  expenses2App.swift
+//  expenses2
+//
+//  Created by Tiger Zhou on 2024-12-28.
+//
+
+import SwiftUI
+
+@main
+struct expenses2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
